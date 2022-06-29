6d4abc01153604348c6ae916d46a0ae53d0f94df
carloscespedes1901 <carlos.cespedes1901@alumnos.ubiobio.cl>
2022-06-29 15:51:50 -0400

    Funcion para crear directorios y subdirectorios con libreria incorporada desde c++17

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "cmake-build-debug\pages.h"
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Crear directorios de prueba: " << std::endl;
+    createFolders("MiPrimerBitvector");
     return 0;
 }
