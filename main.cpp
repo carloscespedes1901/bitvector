f149663e3241bc419a70a1e8d89fd0fb348b2a62
miguelubb <miguelubb@gmail.com>
2022-06-30 17:51:30 -0400

    modificación clase Storage

--- main.cpp
+++ main.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
-#include "Bitvector.h"
+#include "Storage.h"
 
 
 int main() {
-    char buff[512];
-    char buffTest[512];
-    for(int i = 0; i < 256; i++){
-        buff[i] = 'a';
-        buffTest[i] = 'z';
+    uint64_t buff[8];
+    uint64_t buffTest[8];
+    uint64_t readBuff[8]{0,0,0,0,0,0,0,0};
+    for(int i = 0; i < 8; i++){
+        buff[i] = i;
+        buffTest[i] = i*i;
     }
-    Bitvector bitv("carlitos", 512);
+    uint64_t a;
+    Storage<uint64_t> bitv("testing.bin", 8);
+    bitv.open("w");
     for(int i = 0; i < 20; i++){
         std::cout << "Appending (0 bien -1 mal. remplazar por assert): " << bitv.appendPage(buff) << std::endl;
         std::cout << "number of pages: " << bitv.getNumberOfPages() << std::endl;
     }
-    for(int i = 0; i < 512; i++){
-        buff[i] = 0;
-    }
-    std::cout << "contenido de mi buffer: " << buff << std::endl;
-    std::cout << "Leyendo pagina 10 a buffer: " << bitv.readPage(buff, 10) << std::endl;
-    std::cout << "contenido de mi buffer: " << buff << std::endl;
-    for(int i = 0; i < 512; i++){
-        buff[i] = 0;
-    }
+    bitv.close();
+    bitv.open("rw");
+
+    std::cout << "contenido de mi buffer guardado: " << buff << std::endl;
+    std::cout << "Leyendo pagina 10 a buffer: " << bitv.readPage(readBuff, 10) << std::endl;
+    std::cout << "contenido de mi buffer leido: " << readBuff << std::endl;
+
     std::cout << "contenido de mi buffer: " << buff << std::endl;
     std::cout << "escribiendo basura en la pagina 10: " << bitv.writePage(buffTest, 10) << std::endl;
-    for(int i = 0; i < 512; i++){
-        buff[i] = 0;
-    }
-    std::cout << "contenido de mi buffer: " << buff << std::endl;
-    std::cout << "Leyendo pagina 10 a buffer: " << bitv.readPage(buff, 10) << std::endl;
-    std::cout << "contenido de mi buffer: " << buff << std::endl;
+
+    std::cout << "contenido de mi buffer update escritura: " << buffTest << std::endl;
+    std::cout << "Leyendo pagina 10 a buffer: " << bitv.readPage(readBuff, 10) << std::endl;
+    std::cout << "contenido de mi buffer: " << readBuff << std::endl;
+    bitv.close();
     return 0;
 }
